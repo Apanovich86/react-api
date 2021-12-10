7e0513bcf45de03ebc8c809d8b6786b911836ad1
Apanovich86 <apanovich.anna.itec@gmail.com>
2021-12-10 19:47:07 +0200

    Test api/login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/tailwind.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import GuestRoute from "./components/GuestRoute";
+import AuthRoute from "./components/AuthRoute";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <div className="bg-gray-300 h-screen">
+          <GuestRoute path="/login" component={Login} />
+          <AuthRoute path="/profile" component={Profile} />
+        </div>
+      </Layout>
+    </Router>
   );
 }
 
